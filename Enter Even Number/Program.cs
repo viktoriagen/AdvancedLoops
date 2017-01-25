886f7c45fb2f202d5d9f3ac0893135ec36073cbd
viktoriagen <viktoriq.genova@gmail.com>
2017-01-25 11:17:47 +0200

    Programs in C#

---
+++ Enter Even Number/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enter_Even_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter even number:");
+                var n = double.Parse(Console.ReadLine());
+
+                while (n % 2 != 0)
+                {
+                    Console.WriteLine("Invalid number!");
+                    Console.Write("Enter even number:");
+                    n = double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("Even number entered: {0}", n);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Invalid number!");
+                Console.Write("Enter even number:");
+                var n = double.Parse(Console.ReadLine());
+                while (n % 2 != 0)
+                {
+                    Console.WriteLine("Invalid number!");
+                    Console.Write("Enter even number:");
+                    n = double.Parse(Console.ReadLine());
+                }
+                Console.WriteLine("Even number entered: {0}", n);
+            }
+            
+        }
+    }
+}
